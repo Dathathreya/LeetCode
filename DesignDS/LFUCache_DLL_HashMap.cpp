926afff36215fd68f27aeb04f5126219262a08f5
Dathathreya <71897926+Dathathreya@users.noreply.github.com>
2024-11-17 17:58:05 +0530

    Update LFUCache_DLL_HashMap.cpp

--- DesignDS/LFUCache_DLL_HashMap.cpp
+++ DesignDS/LFUCache_DLL_HashMap.cpp
@@ -1,4 +1,70 @@
-  void put(int key, int value) { 
+typedef struct DLL{
+    int key;
+    int value;
+    int count;// to store its occurence
+    struct DLL* prev;
+    struct DLL* next;
+    DLL(int _k,int _v,int _c,struct DLL* _p,struct DLL* _n): key(_k),value(_v),count(_c),prev(_p),next(_n){}
+}_dll;
+
+class LFUCache {
+    private:
+        int capacity; // mp size for cap check    
+        unordered_map<int,_dll*> mp; // key,value pair  - key,LRU_node_address --> capacity should be more or equal to size of this container
+        map<int,pair<_dll*,_dll*>> freq; // count of frequencies - count,LRU_node_address_pairs_of_<head_,tail>
+        // two or more different keys having the same frequency (number of times its got or updated)
+    public:
+        LFUCache(int cap) {
+            capacity = cap;mp.clear();freq.clear();
+        }   
+        int get(int key) {
+            if(mp.find(key)!=mp.end()){ // key is present
+                _dll* addr = mp[key];
+                int cunt = addr->count;
+                int val1 = addr->value;
+                _dll* head=freq[cunt].first;
+                _dll* tail=freq[cunt].second; // reduce list items for count
+                _dll* anxt = addr->next;
+                _dll* aprv = addr->prev;
+                if(aprv!=NULL)
+                    aprv->next = anxt;
+                if(anxt!=NULL)
+                    anxt->prev = aprv;
+                if((head!=NULL)&&(tail!=NULL)&&(head->prev==tail)&&(tail->next==head)){                     
+                    freq.erase(cunt); // drop entry with key having value cunt as count
+                }
+                if(freq.find(cunt+1)==freq.end()){
+                     addr->count+=1;
+                    _dll* head1 = new DLL(-1,-1,-1,NULL,NULL);
+                    _dll* tail1 =  new DLL(-1,-1,-1,NULL,NULL);
+                    head1->prev=tail1;
+                    tail1->next=head1;
+                    _dll* hp=head1->prev;
+                    head1->prev = addr;
+                    hp->next = addr;
+                    addr->next = head1;                    
+                    addr->prev = hp;
+                    freq[cunt+1] = {head1,tail1};
+                }
+                else{
+                    addr->count+=1;
+                    _dll* head1 = freq[cunt+1].first; // increase list items for count
+                    _dll* tail1 = freq[cunt+1].second; 
+                    if(head1!=NULL)
+                    {
+                    _dll* hp=head1->prev;
+                    head1->prev = addr;
+                    hp->next = addr;
+                    addr->next = head1;                    
+                    addr->prev = hp;
+                    }
+                    freq[cunt+1] = {head1,tail1};
+                }
+                return val1;
+            }
+            return -1;
+        }     
+         void put(int key, int value) { 
              if(mp.find(key)!=mp.end()){ // key is present
                 _dll* addr = mp[key];
                 int cunt = addr->count;
@@ -13,7 +79,7 @@
                 if(anxt!=NULL)
                     anxt->prev = aprv;
                 if((head!=NULL)&&(tail!=NULL)&&(head->prev==tail)&&(tail->next==head)){                     
-                    freq.erase(key); // delete from freq that key
+                    freq.erase(cunt); // delete from freq that key
                 }
                  addr->count+=1;
                 if(freq.find( addr->count)==freq.end()){
@@ -100,3 +166,9 @@
         }
 };
 
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
